8f2452465c1a17fb057d9119a200b238b8d9d428
Kamil Dziurdzia <115581395+DziurdziusDZ@users.noreply.github.com>
2026-09-07 12:37:24 +0200

    some changes

--- sync.php
+++ sync.php
@@ -1,7 +1,8 @@
 <?php
-// Ustaw własny, trudny do odgadnięcia klucz
-define('SYNC_TOKEN', 'Bonasus_TajnyKlucz_2026!#');
+define('SYNC_TOKEN', 'b4bc8cba98a587dd933aeaf203528ebd05fd825e0c23cfb617cd53f7360f0d431def7e2d0d5484f33efb21baed209dadba43b45bcf79f9510fcb61e7bc1852081595574ee508c40c3a1588da36ad299ed3b21706c714923d16cfe36d6c7297fd2e825d7fe804494e31902f56380a5a009621453ec92043a00e6d164807240ceb9f9f24fea6c64032bd7ccf1eba58b928c700595ed522402c820f5da3ca621bf3');
 
+
+// dalsza część skryptu pobierająca z Google...
 // Weryfikacja tokenu w adresie URL
 if (!isset($_GET['token']) || $_GET['token'] !== SYNC_TOKEN) {
     http_response_code(403);
